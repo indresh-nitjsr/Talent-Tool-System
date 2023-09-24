fb969abac6a761874a26461351a22c8e291dd296
Indresh <indresh.nitjsr@gmail.com>
2023-09-24 06:25:14 +0530

    header and footer updated

--- Frontend/TalentToolSystem/src/app/header/header.component.ts
+++ Frontend/TalentToolSystem/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faHome, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  home = faHome;
+  search = faSearch;
 }
